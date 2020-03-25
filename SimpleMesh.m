7e75a35c2f002639306356c1baeb8c36c847cbd9
RNakahara <pikumin55@hotmail.com>
2020-03-25 16:43:30 +0900

    回転機能を追加

--- SimpleMesh.m
+++ SimpleMesh.m
@@ -28,13 +28,16 @@ classdef SimpleMesh
             
             % figure関連の設定　メモ：場所の変更を考慮
             figure 
+            %hold on % メモ：指定するとviewが変わるため一時コメントアウト
             xlabel('x_1')
             ylabel('y_1')
             zlabel('z = f(x_1, x_2)')
             
-            % viewのデフォルト設定
             % mesh作成
             mesh(obj.X1, obj.X2, obj.Z)
+            
+            % view 方向の取得
+            % メモ：　x1 x2 ではなくHor(水平)　Ver(垂直)とすべきか
             [obj.view_x1, obj.view_x2] = view;
         end
         
@@ -60,64 +63,103 @@ classdef SimpleMesh
         end
         
        %% 表現方法
-        % メモ： viewを毎回設定する方法は？
+        % メモ： viewを毎回設定しているのはよくない気がする
         
         function mesh(obj)
+            [obj.view_x1, obj.view_x2] = view;
             mesh(obj.X1, obj.X2, obj.Z)
-            %view([view_x1 view_x2])            
+            view([obj.view_x1, obj.view_x2])            
         end
         
         function surf(obj)
+            [obj.view_x1, obj.view_x2] = view;
             surf(obj.X1, obj.X2, obj.Z)
-            view([view_x1 view_x2])            
+            view([obj.view_x1, obj.view_x2])            
         end
         
         function surfWithInterp(obj)
+            [obj.view_x1, obj.view_x2] = view;
             surf(obj.X1, obj.X2, obj.Z)
             shading interp
-            view([view_x1 view_x2])            
+            view([obj.view_x1, obj.view_x2])            
         end
         
         function surfc(obj)
+            [obj.view_x1, obj.view_x2] = view;
             surfc(obj.X1, obj.X2, obj.Z)
             shading interp
-            view([view_x1 view_x2])            
+            view([obj.view_x1, obj.view_x2])            
         end
         
         function contour(obj)
+            [obj.view_x1, obj.view_x2] = view;
             contour(obj.X1, obj.X2, obj.Z)
-            view([view_x1 view_x2])            
+            view([obj.view_x1, obj.view_x2])            
         end
         
         function contourf(obj)
+            [obj.view_x1, obj.view_x2] = view;
             contourf(obj.X1, obj.X2, obj.Z)
-            view([view_x1 view_x2])            
+            view([obj.view_x1, obj.view_x2])            
         end
         
        %% view角度
-        function outputArg = view(obj, view_x1, view_x2)
+        function view(obj, view_x1, view_x2)
             view([view_x1 view_x2])
             obj.view_x1 = view_x1;
             obj.view_x2 = view_x2;
         end
         
-        function outputArg = viewRotate(obj, rotateNum)
-            [view_x1 view_x2] = view;
-            
+        % 回転動作のデモ
+        function viewRotate(obj, rotateNum)
+            for i=1:rotateNum
+                pause(0.01)
+                [obj.view_x1, obj.view_x2] = view;
+                obj.view_x1 = obj.view_x1 + 1;
+                obj.view_x2 = obj.view_x2 + 1;
+                view([obj.view_x1, obj.view_x2])
+            end
         end
         
+        function viewRotateX1(obj, rotateNum)
+            for i=1:rotateNum
+                pause(0.01)
+                [obj.view_x1, obj.view_x2] = view;
+                obj.view_x1 = obj.view_x1 + 1;
+                view([obj.view_x1, obj.view_x2])
+            end
+        end
+        
+        function viewRotateX2(obj, rotateNum)
+            for i=1:rotateNum
+                pause(0.01)
+                [obj.view_x1, obj.view_x2] = view;
+                obj.view_x2 = obj.view_x2 + 1;
+                view([obj.view_x1, obj.view_x2])
+            end
+        end
         
        %% map関係
-        function outputArg = colorMap(obj, map)
+        function colorMap(obj, map)
             colormap (map)
             colorbar
         end
         
-        function outputArg = colorMapJet(obj, jetNum)
+        function colorMapJet(obj, jetNum)
             colormap (jet(jetNum))
             colorbar
         end
         
+       %% 表面に線を描く
+       function lineOnSurface(obj)
+           x1_line = linspace(-1 ,1 ,20);
+           x2_line = linspace(0, 10, 20);
+           z_line = cos(x1_line).*x2_line;
+           
+           hold on
+           plot3(x1_line, x2_line, z_line, 'm', 'LineWidth', 2)
+       end
+       
     end
 end
 
