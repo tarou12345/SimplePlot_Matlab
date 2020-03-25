09f2491bf49823717eb037b2b26b94d2c420916a
RNakahara <pikumin55@hotmail.com>
2020-03-25 17:14:38 +0900

    作成

---
+++ TestClass.m
@@ -0,0 +1,23 @@
+classdef TestClass
+    %TESTCLASS このクラスの概要をここに記述
+    %   詳細説明をここに記述
+    
+    properties
+        x
+        y
+    end
+    
+    methods
+        function obj = TestClass(x,y)
+            % インスタンス
+            obj.x = x;
+            obj.y = y;
+        end
+        
+        function plot(obj)
+            figure
+            plot(obj.x, obj.y);
+        end
+    end
+end
+
